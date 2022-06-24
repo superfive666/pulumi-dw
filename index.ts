8a0853e4f7701216163a76015fc26916e083a265
Wu chao <chao.wu@bitdeer.com>
2022-06-24 13:42:26 +0800

    feature: Added code structures for creating small rds instance for EMR cluster

--- index.ts
+++ index.ts
@@ -1,10 +1,11 @@
 import * as pulumi from "@pulumi/pulumi";
 
 import { configureVpc } from "./src/vpc";
-import { configureIamRole } from "./src/iam";
-import { configureEc2Instance } from "./src/ec2";
-import { configureEmrCluster } from "./src/mer";
+import { configureRds } from "./src/rds";
 import { configureAlbs } from "./src/alb";
+import { configureIamRole } from "./src/iam";
+import { configureEmrCluster } from "./src/mer";
+import { configureEc2Instance } from "./src/ec2";
 
 // GLOBAL config variables
 const env = pulumi.getStack();
@@ -25,13 +26,17 @@ log(
   `EC2 instance for installing tableau completed successfully: ${tableau}`
 );
 
+// Create small RDS instance for EMR to store metadata information
+const rds = configureRds(env);
+log("info", `Small RDS instance created successfully for EMR cluster`);
+
 // Create EMR cluster
 const emr = configureEmrCluster(env);
 log("info", `EMR cluster created successfully`);
 
 // Create relevant ALBs including respective target groups
 const albs = configureAlbs(env);
-log("info", `All ALBs are created successfully`)
+log("info", `All ALBs are created successfully`);
 
 const log = (level: "info" | "warning" | "error", message: string) => {
   const formatted = `${new Date().toISOString()} [${level}]: ${message}`;
