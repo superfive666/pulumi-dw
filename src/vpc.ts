3a55f091a49ee3f24f66ca5ab2f9b0ed7b4c3640
Wu chao <chao.wu@bitdeer.com>
2022-06-24 15:26:09 +0800

    feature: Fine tune the traffic access control for some security groups defined

--- src/vpc.ts
+++ src/vpc.ts
@@ -1,6 +1,7 @@
 import * as pulumi from '@pulumi/pulumi';
 
 import { Tags } from '@pulumi/aws/tags';
+import { ec2 } from '@pulumi/aws/types/input';
 import { SecurityGroup } from '@pulumi/aws/ec2';
 import { Vpc, VpcSubnetArgs } from '@pulumi/awsx/ec2';
 
@@ -13,6 +14,37 @@ interface IVpcConfig {
   numberOfAvailabilityZones: number;
   numberOfNatGateways: number;
 }
+
+const ALLOW_ALL_HTTP: pulumi.Input<ec2.SecurityGroupIngress> = {
+  description: 'Allow all http traffic from anywhere',
+  fromPort: 80,
+  toPort: 80,
+  protocol: 'tcp',
+  cidrBlocks: ['0.0.0.0/0']
+};
+
+const ALLOW_ALL_HTTPS: pulumi.Input<ec2.SecurityGroupIngress> = {
+  description: 'Allow all http traffic from anywhere',
+  fromPort: 443,
+  toPort: 443,
+  protocol: 'tcp',
+  cidrBlocks: ['0.0.0.0/0']
+};
+
+/**
+ * Standard Security Egress Rule
+ * this rule allow all internal resources to be able to access the internet;
+ * modify this rule if it is required to be controlled otherwise
+ */
+const egress: pulumi.Input<ec2.SecurityGroupEgress>[] = [
+  {
+    description: 'Allow all outgoing traffic',
+    fromPort: 0,
+    toPort: 65535,
+    protocol: 'all',
+    cidrBlocks: ['0.0.0.0/0']
+  }
+];
 
 export const configureVpc = (env: string): IVpcDetails => {
   const vpc = createVpc(env);
@@ -40,7 +72,18 @@ const createVpc = (env: string): Vpc => {
     'pulumi:Stack': stack
   };
 
-  const subnets: VpcSubnetArgs[] = [];
+  const subnets: VpcSubnetArgs[] = [
+    {
+      type: 'public',
+      name: `data-${env}-subnet-public`,
+      cidrMask: 24,
+    },
+    {
+      type: 'private',
+      name: `data-${env}-subnet-private`,
+      cidrMask: 24,
+    }
+  ];
 
   const vpc = new Vpc(vpcName, {
     numberOfAvailabilityZones,
@@ -65,11 +108,12 @@ const createSecurityGroups = (env: string): SecurityGroup[] => {
   const alb = new SecurityGroup(`${baseName}-${env}-alb`, {
     description: 'Security group for ALB resource',
     // allow 80 and 443
-    ingress: [],
+    ingress: [ALLOW_ALL_HTTP, ALLOW_ALL_HTTPS],
     tags: {
       ...baseTags,
       purpose: 'alb'
-    }
+    },
+    egress
   });
   const alb2 = new SecurityGroup(`${baseName}-${env}-alb2`, {
     description: 'Security group for internal ALB resource',
@@ -78,7 +122,8 @@ const createSecurityGroups = (env: string): SecurityGroup[] => {
     tags: {
       ...baseTags,
       purpose: 'alb-internal'
-    }
+    },
+    egress
   });
   const emr = new SecurityGroup(`${baseName}-${env}-emr`, {
     description: 'Security group for EMR resource',
@@ -87,16 +132,26 @@ const createSecurityGroups = (env: string): SecurityGroup[] => {
     tags: {
       ...baseTags,
       purpose: 'emr'
-    }
+    },
+    egress
   });
   const rds = new SecurityGroup(`${baseName}-${env}-rds`, {
     description: 'Security group for RDS resource',
     // only allow access from EMR (for storing metadata)
-    ingress: [],
+    ingress: [
+      {
+        description: 'EMR access right',
+        protocol: 'tcp',
+        fromPort: 5432,
+        toPort: 5432,
+        securityGroups: [`${baseName}-${env}-emr`]
+      }
+    ],
     tags: {
       ...baseTags,
       purpose: 'rds'
-    }
+    },
+    egress
   });
   const tableau = new SecurityGroup(`${baseName}-${env}-tableau`, {
     description: 'Security group for EC2 instance that will be installed with Tableau app',
@@ -105,7 +160,8 @@ const createSecurityGroups = (env: string): SecurityGroup[] => {
     tags: {
       ...baseTags,
       purpose: 'tableau'
-    }
+    },
+    egress
   });
 
   return [alb, alb2, emr, rds, tableau];
