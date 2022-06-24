001446d0afd0cccea0e84ea4d3b72d34aaf19daf
Wu chao <chao.wu@bitdeer.com>
2022-06-24 18:02:08 +0800

    feature: Added the EC2 creation steps for the instance responsible for the tableau installation

--- src/ec2.ts
+++ src/ec2.ts
@@ -1,7 +1,19 @@
+import * as pulumi from '@pulumi/pulumi';
+
+import { Instance } from '@pulumi/aws/ec2';
+
 interface IEc2Config {
   instanceType: string;
 }
 
-export const configureEc2Instance = (env: string) => {
+export const configureEc2Instance = (env: string): Instance => {
+  const config = new pulumi.Config();
+  const { instanceType } = config.requireObject<IEc2Config>('ec2');
 
-}
+  const serverName = `data-tableau-${env}`;
+
+  // Pending: AMI, secret key, vpc, subnet, volume, user-data
+  const server = new Instance(serverName, { instanceType });
+
+  return server;
+};
