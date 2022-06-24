dae273df2c17e3a6110f0eab5bf3bf25f01b0a4a
Wu chao <chao.wu@bitdeer.com>
2022-06-24 17:38:31 +0800

    feature: Added the load balancer initial setting, without creating the target groups and listeners

--- index.ts
+++ index.ts
@@ -13,34 +13,38 @@ export const log = (level: 'info' | 'warning' | 'error', message: string) => {
   console.log(formatted);
 };
 
-// GLOBAL config variables
-const env = pulumi.getStack();
-log('info', `Current pulumi environment building is ${env}`);
+const start = async (): Promise<void> => {
+  // GLOBAL config variables
+  const env = pulumi.getStack();
+  log('info', `Current pulumi environment building is ${env}`);
 
-// Create the S3 bucket for EMR to store necessary HDFS files
-const s3 = configureS3Bucket(env);
-log('info', `S3 bucket named ${s3.id} created successfully`);
+  // Create the S3 bucket for EMR to store necessary HDFS files
+  const s3 = configureS3Bucket(env);
+  log('info', `S3 bucket named ${s3.id} created successfully`);
 
-// Create necessary IAM roles for AWS resources
-const roles = configureIamRoles(env);
-log('info', `IAM roles are created: ${roles}`);
+  // Create necessary IAM roles for AWS resources
+  const roles = configureIamRoles(env);
+  log('info', `IAM roles are created: ${roles}`);
 
-// Create required VPC for the data-platform
-const {} = configureVpc(env);
-log('info', `VPC created successfully`);
+  // Create required VPC for the data-platform
+  const {} = configureVpc(env);
+  log('info', `VPC created successfully`);
 
-// Create EC2 instance for installation of Tableau
-const tableau = configureEc2Instance(env);
-log('info', `EC2 instance for installing tableau completed successfully: ${tableau}`);
+  // Create EC2 instance for installation of Tableau
+  const tableau = configureEc2Instance(env);
+  log('info', `EC2 instance for installing tableau completed successfully: ${tableau}`);
 
-// Create small RDS instance for EMR to store metadata information
-const rds = configureRds(env);
-log('info', `Small RDS instance created successfully for EMR cluster: ${rds}`);
+  // Create small RDS instance for EMR to store metadata information
+  const rds = configureRds(env);
+  log('info', `Small RDS instance created successfully for EMR cluster: ${rds}`);
 
-// Create EMR cluster
-const emr = configureEmrCluster(env);
-log('info', `EMR cluster created successfully: ${emr}`);
+  // Create EMR cluster
+  const emr = configureEmrCluster(env);
+  log('info', `EMR cluster created successfully: ${emr}`);
 
-// Create relevant ALBs including respective target groups
-const albs = configureAlbs(env);
-log('info', `All ALBs are created successfully: ${albs}`);
+  // Create relevant ALBs including respective target groups
+  const albs = await configureAlbs();
+  log('info', `All ALBs are created successfully: ${albs}`);
+};
+
+start();
--- src/alb.ts
+++ src/alb.ts
@@ -1,26 +1,40 @@
 import * as pulumi from '@pulumi/pulumi';
 
 import { Tags } from '@pulumi/aws';
+import { Vpc } from '@pulumi/awsx/ec2';
 import { LoadBalancer } from '@pulumi/aws/lb';
+import { SecurityGroup } from '@pulumi/aws/ec2';
 
 const pulumiProject = pulumi.getProject();
 const stack = pulumi.getStack();
-const tags: Tags = {
+const baseTags: Tags = {
   'pulumi:Project': pulumiProject,
   'pulumi:Stack': stack,
   purpose: 'alb'
 };
+
+interface IAlbConfigurationProps {
+  env: string;
+  albSecurityGroup: SecurityGroup;
+  albInternalSecurityGroup: SecurityGroup;
+  vpc: Vpc;
+}
 
 interface IAlbSettings {
   internal: LoadBalancer;
   external: LoadBalancer;
 }
 
-export const configureAlbs = (env: string): IAlbSettings => {
-  const internal = createInternalAlb(env);
-  const external = createExternalAlb(env);
+export const configureAlbs = async ({
+  env,
+  albSecurityGroup,
+  albInternalSecurityGroup,
+  vpc
+}: IAlbConfigurationProps): Promise<IAlbSettings> => {
+  const internal = await createInternalAlb(env, albInternalSecurityGroup, vpc);
+  const external = await createExternalAlb(env, albSecurityGroup, vpc);
 
-  return { internal, external };
+  return Promise.resolve({ internal, external });
 };
 
 /**
@@ -30,7 +44,23 @@ export const configureAlbs = (env: string): IAlbSettings => {
  *
  * @param env The pulumi environment key, such as `dev`
  */
-const createInternalAlb = (env: string): LoadBalancer => {};
+const createInternalAlb = async (env: string, sg: SecurityGroup, vpc: Vpc): Promise<LoadBalancer> => {
+  const lbName = `data-lb-${env}-internal`;
+  const subnets = await vpc.getSubnetsIds('public');
+  const lb = new LoadBalancer(lbName, {
+    internal: true,
+    loadBalancerType: 'application',
+    securityGroups: [sg.arn],
+    enableDeletionProtection: true,
+    subnets,
+    tags: {
+      ...baseTags,
+      'alb-type': 'internal'
+    }
+  });
+
+  return Promise.resolve(lb);
+};
 
 /**
  * External Load Balancer is the standard load balancer for http/https (80,443) traffic.
@@ -38,4 +68,20 @@ const createInternalAlb = (env: string): LoadBalancer => {};
  *
  * @param env The pulumi environment key, such as `dev`
  */
-const createExternalAlb = (env: string): LoadBalancer => {};
+const createExternalAlb = async (env: string, sg: SecurityGroup, vpc: Vpc): Promise<LoadBalancer> => {
+  const lbName = `data-lb-${env}-external`;
+  const subnets = await vpc.getSubnetsIds('public');
+  const lb = new LoadBalancer(lbName, {
+    internal: true,
+    loadBalancerType: 'application',
+    securityGroups: [sg.arn],
+    subnets,
+    enableDeletionProtection: true,
+    tags: {
+      ...baseTags,
+      'alb-type': 'external'
+    }
+  });
+
+  return Promise.resolve(lb);
+};
