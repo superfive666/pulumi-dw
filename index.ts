66deeb00796e393e31cd6bf9120acaa3a50300af
Wu chao <chao.wu@bitdeer.com>
2022-06-27 16:43:42 +0800

    feature: for all pulumi output variables, need to update to use callback to access the output

--- index.ts
+++ index.ts
@@ -5,7 +5,6 @@ import { configureRds } from './src/rds';
 import { configureAlbs } from './src/alb';
 import { configureS3Bucket } from './src/s3';
 import { configureIamRoles } from './src/iam';
-import { configureEmrCluster } from './src/emr';
 import { configureEc2Instance } from './src/ec2';
 
 export const log = (level: 'info' | 'warning' | 'error', message: string) => {
@@ -22,27 +21,47 @@ const start = async (): Promise<void> => {
 
   // Create the S3 bucket for EMR to store necessary HDFS files
   const s3 = configureS3Bucket(env);
-  log('info', `S3 bucket named ${s3.id} created successfully`);
+  s3.id.apply((s3BucketId) => log('info', `S3 bucket ID ${s3BucketId} created successfully`));
 
   // Create necessary IAM roles for AWS resources
   const roles = configureIamRoles(env);
-  log('info', `IAM roles are created: ${roles}`);
+  pulumi.all([roles.alb.arn, roles.emr.arn, roles.rds.arn, roles.tableau.arn]).apply(([alb, emr, rds, tableua]) => {
+    log('info', `IAM role for ALB created with ARN: ${alb}`);
+    log('info', `IAM role for EMR created with ARN: ${emr}`);
+    log('info', `IAM role for RDS created with ARN: ${rds}`);
+    log('info', `IAM role Tableau ALB created with ARN: ${tableua}`);
+  });
 
   // Create required VPC for the data-platform
   const { vpc, securityGroups } = configureVpc(env);
-  log('info', `VPC created successfully, ${vpc}, ${securityGroups}`);
+  pulumi
+    .all([
+      vpc.vpc.arn,
+      securityGroups.alb.arn,
+      securityGroups.alb2.arn,
+      securityGroups.emr.arn,
+      securityGroups.rds.arn,
+      securityGroups.tableau.arn
+    ])
+    .apply(([vpc, alb, alb2, emr, rds, tableau]) => {
+      log('info', `Project APP MPDW VPC created with ARN: ${vpc}`);
+      log('info', `Security group for standard ALB created with ARN: ${alb}`);
+      log('info', `Security group for internal (engineer) ALB created with ARN: ${alb2}`);
+      log('info', `Security group for EMR cluster created with ARN: ${emr}`);
+      log('info', `Security group for RDS metadata created with ARN: ${rds}`);
+      log('info', `Security group for EC2 installing tableau created with ARN: ${tableau}`);
+    });
 
   // Create small RDS instance for EMR to store metadata information
   const rds = await configureRds(env, vpc, roles.rds, securityGroups.rds);
-  log('info', `Small RDS instance created successfully for EMR cluster: ${rds}`);
+  rds.arn.apply((rdsArn) => log('info', `RDS for EMR metatdata created with ARN: ${rdsArn}`));
 
   // Create EC2 instance for installation of Tableau
   const tableau = configureEc2Instance(env);
-  log('info', `EC2 instance for installing tableau completed successfully: ${tableau}`);
+  tableau.arn.apply((ec2Arn) => log('info', `EC2 instance for installing tableau created with ARN: ${ec2Arn}`));
 
   // Create EMR cluster
-  const emr = configureEmrCluster(env);
-  log('info', `EMR cluster created successfully: ${emr}`);
+  // const emr = configureEmrCluster(env);
 
   // Create relevant ALBs including respective target groups
   const albs = await configureAlbs({
@@ -51,7 +70,10 @@ const start = async (): Promise<void> => {
     albInternalSecurityGroup: securityGroups.alb2,
     vpc
   });
-  log('info', `All ALBs are created successfully: ${albs}`);
+  pulumi.all([albs.internal.arn, albs.external.arn]).apply(([internal, external]) => {
+    log('info', `Internal ALB (engineer) created with ARN: ${internal}`);
+    log('info', `External ALB (http/https) created with ARN: ${external}`);
+  });
 };
 
 start();
