49e40818229b7cf4b1d310d0fa8b38e51f7d16e9
Wu chao <chao.wu@bitdeer.com>
2022-06-28 15:33:48 +0800

    fix: adding mysql dependency for creating database user, update config with secrets, update rds deployment steps, added test deployment function for testing

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ export const log = (level: 'info' | 'warning' | 'error', message: string) => {
   console.log(formatted);
 };
 
-const start = async (): Promise<void> => {
+export const start = async (): Promise<void> => {
   log('info', 'Pulumi deployment started...');
 
   // GLOBAL config variables
@@ -56,24 +56,60 @@ const start = async (): Promise<void> => {
   const rds = await configureRds(env, vpc, securityGroups.rds);
   rds.arn.apply((rdsArn) => log('info', `RDS for EMR metatdata created with ARN: ${rdsArn}`));
 
-  // // Create EC2 instance for installation of Tableau
-  // const tableau = configureEc2Instance(env);
-  // tableau.arn.apply((ec2Arn) => log('info', `EC2 instance for installing tableau created with ARN: ${ec2Arn}`));
-  //
-  // // Create EMR cluster
-  // // const emr = configureEmrCluster(env);
-  //
-  // // Create relevant ALBs including respective target groups
-  // const albs = await configureAlbs({
-  //   env,
-  //   albSecurityGroup: securityGroups.alb,
-  //   albInternalSecurityGroup: securityGroups.alb2,
-  //   vpc
-  // });
-  // pulumi.all([albs.internal.arn, albs.external.arn]).apply(([internal, external]) => {
-  //   log('info', `Internal ALB (engineer) created with ARN: ${internal}`);
-  //   log('info', `External ALB (http/https) created with ARN: ${external}`);
-  // });
+  // Create EC2 instance for installation of Tableau
+  const tableau = configureEc2Instance(env);
+  tableau.arn.apply((ec2Arn) =>
+    log('info', `EC2 instance for installing tableau created with ARN: ${ec2Arn}`)
+  );
+
+  // Create EMR cluster
+  // const emr = configureEmrCluster(env);
+
+  // Create relevant ALBs including respective target groups
+  const albs = await configureAlbs({
+    env,
+    albSecurityGroup: securityGroups.alb,
+    albInternalSecurityGroup: securityGroups.alb2,
+    vpc
+  });
+  pulumi.all([albs.internal.arn, albs.external.arn]).apply(([internal, external]) => {
+    log('info', `Internal ALB (engineer) created with ARN: ${internal}`);
+    log('info', `External ALB (http/https) created with ARN: ${external}`);
+  });
 };
 
-start();
+export const testDeploy = async (): Promise<void> => {
+  log('info', 'Pulumi deployment started...');
+
+  // GLOBAL config variables
+  const env = pulumi.getStack();
+  log('info', `Current pulumi environment building is ${env}`);
+
+  // Create required VPC for the data-platform
+  const { vpc, securityGroups } = configureVpc(env);
+  pulumi
+    .all([
+      vpc.vpc.arn,
+      securityGroups.alb.arn,
+      securityGroups.alb2.arn,
+      securityGroups.emr.arn,
+      securityGroups.rds.arn,
+      securityGroups.tableau.arn
+    ])
+    .apply(([vpc, alb, alb2, emr, rds, tableau]) => {
+      log('info', `Project APP MPDW VPC created with ARN: ${vpc}`);
+      log('info', `Security group for standard ALB created with ARN: ${alb}`);
+      log('info', `Security group for internal (engineer) ALB created with ARN: ${alb2}`);
+      log('info', `Security group for EMR cluster created with ARN: ${emr}`);
+      log('info', `Security group for RDS metadata created with ARN: ${rds}`);
+      log('info', `Security group for EC2 installing tableau created with ARN: ${tableau}`);
+    });
+
+  // Create small RDS instance for EMR to store metadata information
+  const rds = await configureRds(env, vpc, securityGroups.rds);
+  rds.arn.apply((rdsArn) => log('info', `RDS for EMR metatdata created with ARN: ${rdsArn}`));
+};
+
+// start();
+
+testDeploy();
--- src/rds.ts
+++ src/rds.ts
@@ -1,51 +1,142 @@
-import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
 import * as awsx from '@pulumi/awsx';
+import * as pulumi from '@pulumi/pulumi';
+import * as mysql from '@pulumi/mysql';
 
 interface IRdsConfig {
-  instanceType: string;
+  engine: string;
+  engineVersion: string;
+  port: number;
+  masterUsername: string;
+  hiveUsername: string;
+  allocatedStorage: number;
+  iamDatabaseAuthenticationEnabled: boolean;
+  instanceClass: string;
 }
 
 export const configureRds = async (
   env: string,
   vpc: awsx.ec2.Vpc,
   sg: aws.ec2.SecurityGroup
-): Promise<aws.rds.Cluster> => {
+): Promise<aws.rds.Instance> => {
   const pulumiProject = pulumi.getProject();
   const stack = pulumi.getStack();
+  const timestamp = new Date().toISOString();
   const tags: aws.Tags = {
     'pulumi:Project': pulumiProject,
     'pulumi:Stack': stack,
     Project: 'mpdw',
-    purpose: 'emr'
+    purpose: 'emr',
+    timestamp
   };
 
   const config = new pulumi.Config();
-  const { instanceType } = config.requireObject<IRdsConfig>('rds');
+  const {
+    instanceClass,
+    engine,
+    engineVersion,
+    port,
+    masterUsername,
+    hiveUsername,
+    allocatedStorage,
+    iamDatabaseAuthenticationEnabled
+  } = config.requireObject<IRdsConfig>('rds');
 
-  const clusterIdentifier = `app-mpdw-rds-${env}`;
-  const dbSubnetGroupName = (await vpc.getSubnetsIds('private'))?.[0];
+  const databaseName = `appmpdwrds${env}`;
+  const password = config.requireSecret('masterPassword');
+  const hivePassword = config.requireSecret('hivePassword');
 
-  const rds = new aws.rds.Cluster('postgresql', {
-    clusterIdentifier,
-    databaseName: clusterIdentifier,
-    engine: 'aurora-postgresql',
-    // DB engine version can be explored via the following commands:
-    // aws rds describe-db-engine-versions --engine aurora-postgresql --query '*[].[EngineVersion]' --output text --region us-west-2
-    engineVersion: '13.6',
+  const dbSubnetGroup = await createDbSubnetGroup(env, vpc, tags);
+  const dbSubnetGroupName = dbSubnetGroup.name;
 
-    vpcSecurityGroupIds: [sg.id],
-    dbSubnetGroupName,
-    dbClusterInstanceClass: instanceType,
+  const rds = new aws.rds.Instance(
+    databaseName,
+    {
+      name: databaseName,
+      engine,
+      // DB engine version can be explored via the following commands:
+      // aws rds describe-db-engine-versions --engine aurora-postgresql --query '*[].[EngineVersion]' --output text --region us-west-2
+      engineVersion,
 
-    // Integrate with AWS KMS for key deployment
-    masterUsername: 'postgres',
-    masterPassword: 'AppMpdwP@ssw0rd',
-    port: 5432,
-    iamDatabaseAuthenticationEnabled: true,
+      vpcSecurityGroupIds: [sg.id],
+      dbSubnetGroupName,
+      instanceClass,
+      allocatedStorage,
 
-    tags
-  });
+      // Integrate with AWS KMS for key deployment
+      username: masterUsername,
+      password: pulumi.interpolate`${password}`,
+      port,
+      iamDatabaseAuthenticationEnabled,
+
+      tags
+    },
+    { dependsOn: [sg, vpc] }
+  );
+
+  createDbUser(rds, hiveUsername, hivePassword, masterUsername, password);
 
   return Promise.resolve(rds);
+};
+
+const createDbSubnetGroup = async (
+  env: string,
+  vpc: awsx.ec2.Vpc,
+  tags: aws.Tags
+): Promise<aws.rds.SubnetGroup> => {
+  const subnetName = `app-mpdw-dbsn-${env}`;
+  const subnetIds = await vpc.getSubnetsIds('private');
+
+  const subnet = new aws.rds.SubnetGroup(
+    subnetName,
+    {
+      subnetIds,
+      tags
+    },
+    { dependsOn: [vpc] }
+  );
+
+  return Promise.resolve(subnet);
+};
+
+const createDbUser = (
+  rds: aws.rds.Instance,
+  hiveUsername: string,
+  hivePassword: pulumi.Output<string>,
+  masterUsername: string,
+  masterPassword: pulumi.Output<string>
+): void => {
+  const provider = new mysql.Provider('mysql', {
+    endpoint: rds.endpoint,
+    username: masterUsername,
+    password: masterPassword
+  });
+
+  const databaseName = 'hive';
+  const database = new mysql.Database(
+    databaseName,
+    {
+      name: databaseName
+    },
+    { provider, dependsOn: [rds, provider] }
+  );
+
+  const user = new mysql.User(
+    hiveUsername,
+    {
+      user: hiveUsername,
+      plaintextPassword: hivePassword
+    },
+    { provider, dependsOn: [rds, database, provider] }
+  );
+
+  new mysql.Grant(
+    hiveUsername,
+    {
+      user: user.user,
+      database: database.name,
+      privileges: ['select', 'insert', 'update', 'delete']
+    },
+    { provider, dependsOn: [user, rds, database, provider] }
+  );
 };
