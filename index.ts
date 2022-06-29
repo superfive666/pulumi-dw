7d294033ef2a0941b5ab06ea7af82465e793805d
Wu chao <chao.wu@bitdeer.com>
2022-06-29 11:22:25 +0800

    fix: Adding exporting output parameters

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ export const log = (level: 'info' | 'warning' | 'error', message: string) => {
   console.log(formatted);
 };
 
-export const start = async (): Promise<void> => {
+export const start = async () => {
   log('info', 'Pulumi deployment started...');
 
   // GLOBAL config variables
@@ -78,7 +78,7 @@ export const start = async (): Promise<void> => {
   });
 };
 
-export const testDeploy = async (): Promise<void> => {
+export const testDeploy = async () => {
   log('info', 'Pulumi deployment started...');
 
   // GLOBAL config variables
@@ -123,6 +123,10 @@ export const testDeploy = async (): Promise<void> => {
   tableau.arn.apply((ec2Arn) =>
     log('info', `EC2 instance for installing tableau created with ARN: ${ec2Arn}`)
   );
+
+  return Promise.resolve({
+    s3, roles, vpc, securityGroups, tableau
+  })
 };
 
 // start();
