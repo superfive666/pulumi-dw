760152dcc1644fea35801317aeaa690ccd9f55f0
Wu chao <chao.wu@bitdeer.com>
2022-07-07 14:28:47 +0800

    feature: Adding account public access block args to the existing s3 setup

--- src/s3.ts
+++ src/s3.ts
@@ -1,6 +1,8 @@
 import * as pulumi from '@pulumi/pulumi';
 import * as aws from '@pulumi/aws';
 
+import { CannedAcl, AccountPublicAccessBlockArgs } from '@pulumi/aws/s3';
+
 interface IS3BucketOutput {
   log: aws.s3.Bucket;
   data: aws.s3.Bucket;
@@ -16,32 +18,63 @@ export const configureS3Bucket = (env: string): IS3BucketOutput => {
     'pulumi:Stack': stack
   };
 
+  const blockOpts: AccountPublicAccessBlockArgs = {
+    blockPublicAcls: true,
+    blockPublicPolicy: true,
+    ignorePublicAcls: true,
+    restrictPublicBuckets: true
+  };
+
   const logName = `app-mpdw-${env}-log`;
   const log = new aws.s3.Bucket(logName, {
-    acl: 'private',
+    acl: CannedAcl.Private,
     tags: {
       ...baseTags,
       purpose: 'emr'
     }
   });
+  new aws.s3.BucketPublicAccessBlock(
+    logName,
+    {
+      ...blockOpts,
+      bucket: log.id
+    },
+    { dependsOn: [log] }
+  );
 
   const dataName = `app-mpdw-${env}-data`;
   const data = new aws.s3.Bucket(dataName, {
-    acl: 'private',
+    acl: CannedAcl.Private,
     tags: {
       ...baseTags,
       purpose: 'emr-data'
     }
   });
+  new aws.s3.BucketPublicAccessBlock(
+    dataName,
+    {
+      ...blockOpts,
+      bucket: data.id
+    },
+    { dependsOn: [data] }
+  );
 
   const jupyterName = `app-mpdw-${env}-jpt`;
   const jupyter = new aws.s3.Bucket(jupyterName, {
-    acl: 'private',
+    acl: CannedAcl.Private,
     tags: {
       ...baseTags,
       purpose: 'emr-jpt'
     }
   });
+  new aws.s3.BucketPublicAccessBlock(
+    jupyterName,
+    {
+      ...blockOpts,
+      bucket: jupyter.id
+    },
+    { dependsOn: [jupyter] }
+  );
 
   return {
     log,
